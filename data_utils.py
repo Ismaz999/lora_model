a582f185063a2bb563e20a68541a1fe925a2fba6
Aziz <aziz.ism98@gmail.com>
2023-10-20 02:22:56 +0200

    Continuation du projet

--- data_utils.py
+++ data_utils.py
@@ -12,7 +12,7 @@ transform_img = transforms.Compose([
     transforms.Resize((256,256)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-                                    ])
+    ])
 
 def build_vocab(captions):
     vocab = {'<start>': 0, '<end>': 1, '<pad>': 2}
